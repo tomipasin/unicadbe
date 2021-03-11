e4d6bdcfe5b23e865f578bab690ef531f1b26398
tomipasin <tomipasin@gmail.com>
2021-03-11 17:03:13 -0300

    v1.1

---
+++ src/database/config.js
@@ -0,0 +1,10 @@
+//aqui as configs de conexão com o bando de dados.Eu estou usando um db
+//noSQL Mongo DB, neste caso a versão cloud, o Atlas, mas tu pode usar
+//uma versão local ou remota que seja da tua preferência. 
+
+module.exports = {
+    uri: "mongodb+srv://tomipasin:ExDBUSer@mflix.nopvf.mongodb.net/entregas?retryWrites=true&w=majority"
+  };
+
+
+  
